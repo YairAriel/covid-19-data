34d7866dbc319ba27d56d2f428746017946bb6b1
YairAriel <yair009@gmail.com>
2020-05-27 23:00:18 +0300

    set styled-components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Summary from './components/Sumarry';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Summary />
     </div>
   );
 }
+
+const AppContainer = styled.div`
+  background: #d9cfcf;
+  height: 100vh;
+  width: 100vw;
+`;
 
 export default App;
