3d7ac6beb5df8a39463263fef7444d1d75723af9
YairAriel <yair009@gmail.com>
2020-06-21 22:52:18 +0300

    set an header for the app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Summary from './components/Sumarry';
 function App() {
   return (
     <AppContainer>
+      <Header>COVID-19 DATA</Header>
       <Summary />
     </AppContainer>
   );
@@ -19,6 +20,13 @@ const AppContainer = styled.div`
     padding: 0 2vw;
     width: unset;
   }
+`;
+
+const Header = styled.h1`
+  color: var(--bronze);
+  margin: 0 auto;
+  text-align: center;
+  padding-top: 2vw;
 `;
 
 export default App;
